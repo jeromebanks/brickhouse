6577e5de7cfa6396ecaed20de69ec9a4c6fa3cb6
Felipe Oliveira <felipera@gmail.com>
2015-12-22 15:50:14 -0800

    Adding parameters to date range udf period type (millis, seconds, minutes, hours, days, months, years) and date format pattern
    Signed-off-by: Felipe Oliveira <felipera@gmail.com>

--- src/main/java/brickhouse/udf/date/DateRangeUDTF.java
+++ src/main/java/brickhouse/udf/date/DateRangeUDTF.java
@@ -1,4 +1,5 @@
 package brickhouse.udf.date;
+
 /**
  * Copyright 2012 Klout, Inc
  *
@@ -16,6 +17,10 @@ package brickhouse.udf.date;
  *
  **/
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
@@ -27,109 +32,151 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.IntObjectInspecto
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
+import org.joda.time.DurationFieldType;
 import org.joda.time.format.DateTimeFormatter;
 
-import java.util.ArrayList;
-
 /**
- * DateRange is a UDTF for generating days
- * from start date to end_date, inclusively.
+ * DateRange is a UDTF for generating days from start date to end_date,
+ * inclusively.
  * <p/>
- * This might be useful in cases where one needed
- * to have a row for every day in the range ...
+ * This might be useful in cases where one needed to have a row for every day in
+ * the range ...
  * <p/>
- * select
- * t1.id,
- * t1.date,
- * coalesce( t2.val, 0.0 ) as val
- * from
- * ( select id, rng.date
- * from tab1
- * lateral view( date_range( tab1.start_date, tab1.end_date ) ) rng as date, index
- * ) t1
- * left outer join
- * ( select val from tab2 ) t2
- * on ( t1.id = t2.id and t1.date = t2.date );
+ * select t1.id, t1.date, coalesce( t2.val, 0.0 ) as val from ( select id,
+ * rng.date from tab1 lateral view( date_range( tab1.start_date, tab1.end_date )
+ * ) rng as date, index ) t1 left outer join ( select val from tab2 ) t2 on (
+ * t1.id = t2.id and t1.date = t2.date );
  */
-@Description(name = "date_range",
-        value = "_FUNC_(a,b,c) - Generates a range of integers from a to b incremented by c"
-                + " or the elements of a map into multiple rows and columns ")
+@Description(name = "date_range", value = "_FUNC_(a,b,c) - Generates a range of integers from a to b incremented by c"
+		+ " or the elements of a map into multiple rows and columns ")
 public class DateRangeUDTF extends GenericUDTF {
-    private static DateTimeFormatter YYMMDD = DateTimeFormat.forPattern("YYYYMMdd");
-    private StringObjectInspector startInspector = null;
-    private StringObjectInspector endInspector = null;
-    private IntObjectInspector incrInspector = null;
 
-    @Override
-    public StructObjectInspector initialize(ObjectInspector[] argOIs)
-            throws UDFArgumentException {
-        if (argOIs.length < 2 || argOIs.length > 3) {
-            throw new UDFArgumentException("DateRange takes <startdate>, <enddate>, <optional increment>");
-        }
+	private StringObjectInspector startInspector = null;
+	private StringObjectInspector endInspector = null;
+	private IntObjectInspector incrInspector = null;
+	private StringObjectInspector durationTypeInspector = null;
+	private StringObjectInspector dateFormatInspector = null;
 
-        if (!(argOIs[0] instanceof StringObjectInspector))
-            throw new UDFArgumentException("DateRange takes <startdate>, <enddate>, <optional increment>");
-        else
-            startInspector = (StringObjectInspector) argOIs[0];
+	// Unfortunately joda doesn't have anything a method where I can do
+	// plus(timeUnit, incr)
+	// In Java 8 we could use ChronoUnit but I don't want to add that dependency
+	// yet
+	static Map<String, DurationFieldType> durationTypes = new HashMap<String, DurationFieldType>();
+	static {
+		durationTypes.put("millis", DurationFieldType.millis());
+		durationTypes.put("seconds", DurationFieldType.seconds());
+		durationTypes.put("minutes", DurationFieldType.minutes());
+		durationTypes.put("hours", DurationFieldType.hours());
+		durationTypes.put("days", DurationFieldType.days());
+		durationTypes.put("months", DurationFieldType.months());
+		durationTypes.put("years", DurationFieldType.years());
+	}
 
-        if (!(argOIs[1] instanceof StringObjectInspector))
-            throw new UDFArgumentException("DateRange takes <startdate>, <enddate>, <optional increment>");
-        else
-            endInspector = (StringObjectInspector) argOIs[1];
+	@Override
+	public StructObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
 
-        if (argOIs.length == 3) {
-            if (!(argOIs[2] instanceof IntObjectInspector))
-                throw new UDFArgumentException("DateRange takes <startdate>, <enddate>, <optional increment>");
-            else
-                incrInspector = (IntObjectInspector) argOIs[2];
-        }
+		String usage = "DateRange takes <startdate>, <enddate>, <optional increment>, <optional durationtype>, <optional dateformat>";
 
+		if ((argOIs.length < 2) || (argOIs.length > 5)) {
+			throw new UDFArgumentException(usage);
+		}
 
-        ArrayList<String> fieldNames = new ArrayList<String>();
-        fieldNames.add("date");
-        fieldNames.add("index");
-        ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
-        fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
-        fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
+		if (!(argOIs[0] instanceof StringObjectInspector)) {
+			throw new UDFArgumentException(usage);
+		} else {
+			this.startInspector = (StringObjectInspector) argOIs[0];
+		}
 
-        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,
-                fieldOIs);
-    }
+		if (!(argOIs[1] instanceof StringObjectInspector)) {
+			throw new UDFArgumentException(usage);
+		} else {
+			this.endInspector = (StringObjectInspector) argOIs[1];
+		}
 
-    private final Object[] forwardListObj = new Object[2];
+		if (argOIs.length >= 3) {
+			if (!(argOIs[2] instanceof IntObjectInspector)) {
+				throw new UDFArgumentException(usage);
+			} else {
+				this.incrInspector = (IntObjectInspector) argOIs[2];
+			}
+		}
 
-    @Override
-    public void process(Object[] args) throws HiveException {
-        String start = null;
-        String end = null;
-        int incr = 1;
-        switch (args.length) {
-            case 3:
-                incr = incrInspector.get(args[2]);
-            case 2:
-                start = startInspector.getPrimitiveJavaObject(args[0]);
-                end = endInspector.getPrimitiveJavaObject(args[1]);
-                break;
-        }
-        try {
-            DateTime startDt = YYMMDD.parseDateTime(start);
-            DateTime endDt = YYMMDD.parseDateTime(end);
-            int i = 0;
-            for (DateTime dt = startDt; dt.isBefore(endDt) || dt.isEqual(endDt); dt = dt.plusDays(incr), i++) {
-                forwardListObj[0] = YYMMDD.print(dt);
-                forwardListObj[1] = new Integer(i);
+		if (argOIs.length >= 4) {
+			if (!(argOIs[3] instanceof StringObjectInspector)) {
+				throw new UDFArgumentException(usage);
+			} else {
+				this.durationTypeInspector = (StringObjectInspector) argOIs[3];
+			}
+		}
 
-                forward(forwardListObj);
-            }
-        } catch (IllegalArgumentException badFormat) {
-            throw new HiveException("Unable to parse dates; start = " + start + " ; end = " + end);
-        }
-    }
+		if (argOIs.length >= 5) {
+			if (!(argOIs[4] instanceof StringObjectInspector)) {
+				throw new UDFArgumentException(usage);
+			} else {
+				this.dateFormatInspector = (StringObjectInspector) argOIs[4];
+			}
+		}
 
-    @Override
-    public void close() throws HiveException {
+		ArrayList<String> fieldNames = new ArrayList<String>();
+		fieldNames.add("date");
+		fieldNames.add("index");
+		ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+		fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+		fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
 
-    }
+		return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+	}
+
+	private final Object[] forwardListObj = new Object[2];
+
+	@Override
+	public void process(Object[] args) throws HiveException {
+		String start = null;
+		String end = null;
+		int incr = 1;
+		DurationFieldType durationType = DurationFieldType.days();
+		DateTimeFormatter dateFormatter = org.joda.time.format.DateTimeFormat.forPattern("YYYYMMdd");
+
+		if (args.length >= 2) {
+			start = this.startInspector.getPrimitiveJavaObject(args[0]);
+			end = this.endInspector.getPrimitiveJavaObject(args[1]);
+		}
+
+		if (args.length >= 3) {
+			incr = this.incrInspector.get(args[2]);
+		}
+
+		if (args.length >= 4) {
+			String value = this.durationTypeInspector.getPrimitiveJavaObject(args[3]);
+			if ((value != null) && durationTypes.containsKey(value.toLowerCase())) {
+				durationType = durationTypes.get(value.toLowerCase());
+			}
+		}
+
+		if (args.length >= 5) {
+			dateFormatter = org.joda.time.format.DateTimeFormat.forPattern(this.dateFormatInspector
+					.getPrimitiveJavaObject(args[4]));
+		}
+
+		try {
+			DateTime startDt = dateFormatter.parseDateTime(start);
+			DateTime endDt = dateFormatter.parseDateTime(end);
+			int i = 0;
+			for (DateTime dt = startDt; dt.isBefore(endDt) || dt.isEqual(endDt); dt = dt.withFieldAdded(
+					durationType, incr), i++) {
+				this.forwardListObj[0] = dateFormatter.print(dt);
+				this.forwardListObj[1] = new Integer(i);
+
+				this.forward(this.forwardListObj);
+			}
+		} catch (IllegalArgumentException badFormat) {
+			throw new HiveException("Unable to parse dates; start = " + start + " ; end = " + end);
+		}
+	}
+
+	@Override
+	public void close() throws HiveException {
+
+	}
 
 }
